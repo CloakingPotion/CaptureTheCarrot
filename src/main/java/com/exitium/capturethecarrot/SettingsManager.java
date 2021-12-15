07eae892bbb81e42b336fd7ff3499c69ea4c1bff
Shawn Whitaker <shawn.w8465@gmail.com>
2021-12-15 10:18:24 -0700

    Cleaned up code a bit
    - Added test server and target to gitignore
    - Deleted test-server folder
    - Adds api version to 1.18
    - Updated artifact name to capture-the-carrot from CaptureTheCarrot
    - Added sourceEncoding to avoid warning from IntelliJ
    - Updated output directory to not include file name as it created a folder with the file name
    - Added some null checks in Settings Manager to avoid running through unnecessary code
    - Modified .gitignore to not have errors (hopefully)

--- src/main/java/com/exitium/capturethecarrot/SettingsManager.java
+++ src/main/java/com/exitium/capturethecarrot/SettingsManager.java
@@ -30,42 +30,58 @@ public class SettingsManager {
 	private FileConfiguration config;
 	
 	private SettingsManager(String fileName) {
-		if(!Main.getPlugin().getDataFolder().exists()) {
+		if (!Main.getPlugin().getDataFolder().exists()) {
 			Main.getPlugin().getDataFolder().mkdir();
 		}
-		
+
 		file = new File(Main.getPlugin().getDataFolder(), fileName + ".yml");
-		
+
 		if (!file.exists()) {
-			try{
+			try {
 				file.createNewFile();
-			}catch (Exception e) {
+			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
-		
-		config = YamlConfiguration.loadConfiguration(file);
+		if (config.getKeys(true).size() == 0) {
+			// This allows me to know that there is nothing in the file
+			// And I can add null checks to config methods to avoid
+			// unnecessary code.
+			config = null;
+		} else {
+			config = YamlConfiguration.loadConfiguration(file);
+		}
 	}
-	
+
 	@SuppressWarnings("unchecked")
 	public <T> T get(String path) {
+		if (config == null) { return null; }
+
 		return (T) config.get(path);
 	}
 	
 	public Set<String> getKeys() {
+		if (config == null) { return null; }
+
 		return config.getKeys(false);
 	}
 	
 	public void set(String path, Object value) {
+		if (config == null) { return; }
+
 		config.set(path, value);
 		save();
 	}
 	
 	public boolean containsStringPath(String path) {
+		if (config == null) { return false; }
+
 		return config.contains(path);
 	}
 	
 	public ConfigurationSection createSection(String path) {
+		if (config == null) { return null; }
+
 		ConfigurationSection section = config.createSection(path);
 		save();
 		return section;
@@ -73,6 +89,8 @@ public class SettingsManager {
 	}
 	
 	private void save() {
+		if (config == null) { return; }
+
 		try{
 			config.save(file);
 		} catch (Exception e) {
--- test-server/start.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-java -Xms3G -Xmx3G -XX:+UseG1GC -jar spigot-1.18.jar nogui
-pause
